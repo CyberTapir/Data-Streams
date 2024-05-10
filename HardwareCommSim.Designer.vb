c179d61e1b8499affa0ef2b57b9d4689db2b56f5
Callum <callumtbennett@gmail.com>
2024-05-10 11:43:35 +1000

    Initial commit

---
+++ HardwareCommSim.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HardwareCommSim
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        txtInput = New TextBox()
+        txtHeader = New TextBox()
+        txtLength = New TextBox()
+        txtData = New TextBox()
+        txtFormat = New TextBox()
+        txtTrailer = New TextBox()
+        txtCRC = New TextBox()
+        lblHeader = New Label()
+        lblLength = New Label()
+        lblFormat = New Label()
+        lblData = New Label()
+        lblTrailer = New Label()
+        lblCRC = New Label()
+        btnProcess = New Button()
+        btnEnd = New Button()
+        SuspendLayout()
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Location = New Point(84, 12)
+        txtInput.Name = "txtInput"
+        txtInput.PlaceholderText = "copy and paste input string here"
+        txtInput.Size = New Size(500, 23)
+        txtInput.TabIndex = 0
+        ' 
+        ' txtHeader
+        ' 
+        txtHeader.Location = New Point(84, 53)
+        txtHeader.Name = "txtHeader"
+        txtHeader.Size = New Size(500, 23)
+        txtHeader.TabIndex = 1
+        ' 
+        ' txtLength
+        ' 
+        txtLength.Location = New Point(84, 79)
+        txtLength.Name = "txtLength"
+        txtLength.Size = New Size(500, 23)
+        txtLength.TabIndex = 2
+        ' 
+        ' txtData
+        ' 
+        txtData.Location = New Point(84, 118)
+        txtData.Name = "txtData"
+        txtData.Size = New Size(500, 23)
+        txtData.TabIndex = 3
+        ' 
+        ' txtFormat
+        ' 
+        txtFormat.Location = New Point(84, 144)
+        txtFormat.Name = "txtFormat"
+        txtFormat.Size = New Size(500, 23)
+        txtFormat.TabIndex = 4
+        ' 
+        ' txtTrailer
+        ' 
+        txtTrailer.Location = New Point(84, 182)
+        txtTrailer.Name = "txtTrailer"
+        txtTrailer.Size = New Size(500, 23)
+        txtTrailer.TabIndex = 5
+        ' 
+        ' txtCRC
+        ' 
+        txtCRC.Location = New Point(84, 208)
+        txtCRC.Name = "txtCRC"
+        txtCRC.Size = New Size(500, 23)
+        txtCRC.TabIndex = 6
+        ' 
+        ' lblHeader
+        ' 
+        lblHeader.AutoSize = True
+        lblHeader.Location = New Point(36, 57)
+        lblHeader.Name = "lblHeader"
+        lblHeader.Size = New Size(45, 15)
+        lblHeader.TabIndex = 7
+        lblHeader.Text = "Header"
+        ' 
+        ' lblLength
+        ' 
+        lblLength.AutoSize = True
+        lblLength.Location = New Point(12, 83)
+        lblLength.Name = "lblLength"
+        lblLength.Size = New Size(71, 15)
+        lblLength.TabIndex = 8
+        lblLength.Text = "Data Length"
+        ' 
+        ' lblFormat
+        ' 
+        lblFormat.AutoSize = True
+        lblFormat.Location = New Point(7, 148)
+        lblFormat.Name = "lblFormat"
+        lblFormat.Size = New Size(76, 15)
+        lblFormat.TabIndex = 9
+        lblFormat.Text = "Format Code"
+        ' 
+        ' lblData
+        ' 
+        lblData.AutoSize = True
+        lblData.Location = New Point(50, 122)
+        lblData.Name = "lblData"
+        lblData.Size = New Size(31, 15)
+        lblData.TabIndex = 9
+        lblData.Text = "Data"
+        ' 
+        ' lblTrailer
+        ' 
+        lblTrailer.AutoSize = True
+        lblTrailer.Location = New Point(43, 186)
+        lblTrailer.Name = "lblTrailer"
+        lblTrailer.Size = New Size(38, 15)
+        lblTrailer.TabIndex = 10
+        lblTrailer.Text = "Trailer"
+        ' 
+        ' lblCRC
+        ' 
+        lblCRC.AutoSize = True
+        lblCRC.Location = New Point(51, 212)
+        lblCRC.Name = "lblCRC"
+        lblCRC.Size = New Size(30, 15)
+        lblCRC.TabIndex = 11
+        lblCRC.Text = "CRC"
+        ' 
+        ' btnProcess
+        ' 
+        btnProcess.Location = New Point(176, 259)
+        btnProcess.Name = "btnProcess"
+        btnProcess.Size = New Size(75, 23)
+        btnProcess.TabIndex = 12
+        btnProcess.Text = "&Process"
+        btnProcess.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnd
+        ' 
+        btnEnd.Location = New Point(326, 259)
+        btnEnd.Name = "btnEnd"
+        btnEnd.Size = New Size(75, 23)
+        btnEnd.TabIndex = 13
+        btnEnd.Text = "&End"
+        btnEnd.UseVisualStyleBackColor = True
+        ' 
+        ' HardwareCommSim
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(603, 301)
+        Controls.Add(btnEnd)
+        Controls.Add(btnProcess)
+        Controls.Add(lblCRC)
+        Controls.Add(lblTrailer)
+        Controls.Add(lblData)
+        Controls.Add(lblFormat)
+        Controls.Add(lblLength)
+        Controls.Add(lblHeader)
+        Controls.Add(txtCRC)
+        Controls.Add(txtTrailer)
+        Controls.Add(txtFormat)
+        Controls.Add(txtData)
+        Controls.Add(txtLength)
+        Controls.Add(txtHeader)
+        Controls.Add(txtInput)
+        Name = "HardwareCommSim"
+        Text = "Hardware Communications Simulator"
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents txtHeader As TextBox
+    Friend WithEvents txtLength As TextBox
+    Friend WithEvents txtData As TextBox
+    Friend WithEvents txtFormat As TextBox
+    Friend WithEvents txtTrailer As TextBox
+    Friend WithEvents txtCRC As TextBox
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents lblLength As Label
+    Friend WithEvents lblFormat As Label
+    Friend WithEvents lblData As Label
+    Friend WithEvents lblTrailer As Label
+    Friend WithEvents lblCRC As Label
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents btnEnd As Button
+End Class
---
+++ HardwareCommSim.vb
@@ -0,0 +1,116 @@
+﻿Public Class HardwareCommSim
+
+    Public Function ConvertDecimalToBinary(ByVal Value As String, Optional ByVal x As Integer = 0) As String
+        Dim iVal#, temp#, ret%, i%, Str$
+        Dim BinVal%()
+
+        iVal = Value
+        Do
+            temp = iVal / 2
+            ret = InStr(temp, ".")
+            If ret > 0 Then
+                temp = Strings.Left(temp, ret - 1)
+            End If
+            ret = iVal Mod 2
+            ReDim Preserve BinVal(i)
+            BinVal(i) = ret
+            i = i + 1
+            iVal = temp
+        Loop While temp > 0
+        For i = UBound(BinVal) To 0 Step -1
+            Str = Str + CStr(BinVal(i))
+        Next
+        If x = 3 Then
+            Select Case Len(Str) Mod 3
+                Case 1
+                    Str = "00" + Str
+                Case 2
+                    Str = "0" + Str
+            End Select
+        ElseIf x = 4 Then
+            Select Case Len(Str) Mod 4
+                Case 1
+                    Str = "000" + Str
+                Case 2
+                    Str = "00" + Str
+                Case 3
+                    Str = "0" + Str
+            End Select
+        End If
+        ConvertDecimalToBinary = Str
+
+    End Function
+
+    Public Function ConvertBinaryToDecimal(ByVal BinVal As String) As String
+        Dim iVal#, temp#, i%, Length%
+
+        Length = Len(BinVal)
+        For i = 0 To Length - 1
+            temp = CInt(Mid(BinVal, Length - i, 1))
+            iVal = iVal + (temp * (2 ^ i))
+        Next i
+        ConvertBinaryToDecimal = iVal
+    End Function
+
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+        Dim strInput As String = txtInput.Text
+        Dim header As String = Strings.Left(strInput, 8)
+        Dim startBits As String = Strings.Left(header, 3)
+        Dim dataBlockLen As String = Strings.Right(header, 5)
+        Dim trailer As String = Strings.Right(strInput, 8)
+        Dim crc As String = Strings.Left(trailer, 4)
+        Dim endBits As String = Strings.Right(trailer, 4)
+        Dim len As Integer = ConvertBinaryToDecimal(CInt(dataBlockLen))
+        Dim controlCharacters As String = strInput.Substring(8, 2)
+        Dim dataBlock As String = strInput.Substring(10, 7 * len)
+        Dim crcCheck As Integer
+        Dim crcValue As Integer
+        For i = 0 To dataBlock.Length - 1
+            If dataBlock(i) = "1" Then
+                crcCheck += 1
+            End If
+        Next i
+        For i = 0 To 1
+            If controlCharacters(i) = "1" Then
+                crcCheck += 1
+            End If
+        Next i
+        crcValue = crcCheck Mod 13
+        If startBits = "101" Then
+            If crcValue = ConvertBinaryToDecimal(crc) Then
+                txtHeader.Text = header
+                txtLength.Text = dataBlockLen
+                txtTrailer.Text = trailer
+                txtCRC.Text = crc
+                txtLength.Text = len
+                txtData.Text = dataBlock
+                txtFormat.Text = controlCharacters
+                Select Case controlCharacters
+                    Case "00"
+                        'no special formatting
+                    Case "01"
+                        'bold
+                        txtData.Font = New Font(txtData.Font, FontStyle.Bold)
+                    Case "10"
+                        'italic
+                        txtData.Font = New Font(txtData.Font, FontStyle.Italic)
+                    Case "11"
+                        '20pt font size
+                        txtData.Font = New Font(txtData.Font.FontFamily, 20)
+                End Select
+                For i = 0 To len - 1
+                    Dim data As String ' first 7 bits
+                    Dim decimalVal As Integer
+                    Dim outString As String
+                    data = dataBlock.Substring(i * 7, 7)
+                    decimalVal = ConvertBinaryToDecimal(CInt(data))
+                    outString = outString & Chr(decimalVal)
+                    txtData.Text = outString
+                Next i
+            End If
+        End If
+    End Sub
+    Private Sub btnEnd_Click(sender As Object, e As EventArgs) Handles btnEnd.Click
+        Me.Close()
+    End Sub
+End Class
